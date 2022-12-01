d315ee59f1bc40d92ba4dfba2b18b495400c93de
jordanag35 <39064199+jordanag35@users.noreply.github.com>
2022-12-01 00:07:02 -0500

    final myClasses.php file fix

--- myClasses.php
+++ myClasses.php
@@ -83,8 +83,10 @@ $statement->closeCursor();
  <div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
    <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
      <a href="#" class="w3-bar-item w3-button" style="width:25% !important">HOME</a>
-     <a href="Myclasses.html" class="w3-bar-item w3-button" style="width:25% !important">My Classes</a>
-     <a href= "Messages.html"class="w3-bar-item w3-button" style="width:25% !important">Messages</a>
+     <a href="index.php" class="w3-bar-item w3-button" style="width:25% !important">My Classes</a>
+     <a href= "myClasses.php"class="w3-bar-item w3-button" style="width:25% !important">Messages</a>
+     <a href= "createClass.php"class="w3-bar-item w3-button" style="width:25% !important">Messages</a>
+
    </div>
  </div>
 
