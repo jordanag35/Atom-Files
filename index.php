d235416b59a66da7b513d0130a5b8de2b79ffe71
jordanag35 <39064199+jordanag35@users.noreply.github.com>
2022-11-30 23:59:16 -0500

    updated index.php file attempt #3

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ button {
     <button class="fa fa-home w3-xxlarge"></button>
     <p>HOME</p>
   </a>
-  <a href="Myclasses.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+  <a href="myClasses.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
     <button class="fa fa-user w3-xxlarge"></button>
     <p>MY CLASSES</p>
   </a>
@@ -78,7 +78,7 @@ button {
 <div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
   <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
     <a href="#" class="w3-bar-item w3-button" style="width:25% !important">HOME</a>
-    <a href="Myclasses.php" class="w3-bar-item w3-button" style="width:25% !important">My Classes</a>
+    <a href="myClasses.php" class="w3-bar-item w3-button" style="width:25% !important">My Classes</a>
     <a href= "Messages.php"class="w3-bar-item w3-button" style="width:25% !important">Messages</a>
     <a href= "createClass.php"class="w3-bar-item w3-button" style="width:25% !important">Messages</a>
   </div>
