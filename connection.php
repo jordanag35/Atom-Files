7f6bc34790beab009747c7f7df466477f8a0cdab
jordanag35 <39064199+jordanag35@users.noreply.github.com>
2022-11-30 23:50:44 -0500

    possible fix attempt #2

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
 
-$dbhost = "141.215.80.154";
-$dbuser = "<group3>";
-$dbpass = "password";
+$servername = "141.215.80.154";
+$username = "<group3>";
+$password = "password";
 $dbname = "group3_db";
 
-if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+if(!$con = mysqli_connect($servername,$username,$password,$dbname))
 {
 
     die("failed to connect!");
